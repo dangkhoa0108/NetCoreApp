2c45cc16e0f32c94deaad388f72d8132d8e8371f
Trần Đăng Khoa <tdangkhoa.itute@gmail.com>
2018-09-11 01:03:03 +0700

    Create Data Access Layer
    Create Data Access Layer

---
+++ CoreApp.Data.EF/EFUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using CoreApp.Infrastructure.Interfaces;
+
+namespace CoreApp.Data.EF
+{
+    public class EFUnitOfWork:IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public EFUnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ CoreApp.Infarstructure/Interfaces/IRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CoreApp.Infrastructure.Interfaces
+{
+    public interface IRepository<T, K> where T:class
+    {
+        /// <summary>
+        /// Get object T with Id K, after do Function with params object T and return object, with other entity
+        /// </summary>
+        /// <param name="id">ID has Generic object</param>
+        /// <param name="includeProperties">Object return (optional)</param>
+        /// <returns>Object T</returns>
+        T FindById(K id, params Expression<Func<T, object>>[] includeProperties);
+
+        /// <summary>
+        /// Get object T with Id K, after do Function with params object T and return object and predicate, with other entity
+        /// </summary>
+        /// <param name="predicate">Condition</param>
+        /// <param name="includeProperties">Object return (optional)</param>
+        /// <returns></returns>
+        T FindSingle(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+
+        /// <summary>
+        /// Get IQueryable object T after do Function with params object T and return object, with other entity
+        /// </summary>
+        /// <param name="includeProperties">Object return (optional)</param>
+        /// <returns></returns>
+        IQueryable<T> FindAll(params Expression<Func<T, object>>[] includeProperties);
+
+        /// <summary>
+        /// Get IQueryable object T after do Function with params object T and return object and predicate, with other entity
+        /// </summary>
+        /// <param name="predicate">Condition</param>
+        /// <param name="includeProperties">Object return (optional)</param>
+        /// <returns></returns>
+        IQueryable<T> FindAll(Expression<Func<T, bool>> predicate ,params Expression<Func<T, object>>[] includeProperties);
+
+        /// <summary>
+        /// Add new entity T
+        /// </summary>
+        /// <param name="entity">Entity input</param>
+        void Add(T entity);
+
+        /// <summary>
+        /// Update entity
+        /// </summary>
+        /// <param name="entity">Entity input</param>
+        void Update(T entity);
+
+        /// <summary>
+        /// Remove entity with entity
+        /// </summary>
+        /// <param name="entity">Entity input</param>
+        void Remove(T entity);
+
+        /// <summary>
+        /// Remove Entity with id
+        /// </summary>
+        /// <param name="id">ID</param>
+        void Remove(K id);
+
+        /// <summary>
+        /// Remove multiple entity
+        /// </summary>
+        /// <param name="entities">List Entity</param>
+        void RemoveMultiple(List<T> entities);
+    }
+}
