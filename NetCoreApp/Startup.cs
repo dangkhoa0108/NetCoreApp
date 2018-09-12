00726001bc98894d5bed754fb342d2f625557f8b
Khoa Tran Dang <tdangkhoa.itute@gmail.com>
2018-09-12 10:56:40 +0700

    Create Area Admin
    Create Area Admin

--- NetCoreApp/Startup.cs
+++ NetCoreApp/Startup.cs
@@ -97,6 +97,10 @@ namespace NetCoreApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name : "areas",
+                    template : "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
