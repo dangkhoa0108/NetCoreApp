627b45e41f8aa074035c1fc1d9b21bd515eb64b2
Khoa Tran Dang <tdangkhoa.itute@gmail.com>
2018-09-18 10:59:13 +0700

    Show Category in tree view

---
+++ NetCoreApp/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,30 @@
+﻿using CoreApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreApp.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : BaseController
+    {
+        private readonly IProductCategoryService _productCategoryService;
+
+        public ProductCategoryController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Get Category API
+
+        public IActionResult GetProductCategory()
+        {
+            var model = _productCategoryService.GetAll();
+            return new OkObjectResult(model);
+        }
+
+        #endregion
+    }
+}
