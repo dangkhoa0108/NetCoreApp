2af9ccc27f179b8692b7d8cea9bceef32be6dce7
Trần Đăng Khoa <tdangkhoa.itute@gmail.com>
2018-09-26 21:02:32 +0700

    Common value
    Common value

--- CoreApp.Utilities/Constants/CommonConstants.cs
+++ CoreApp.Utilities/Constants/CommonConstants.cs
@@ -4,5 +4,7 @@
     {
         public const string DefaultFooterId = "DefaultFooterId";
         public const string Admin = "Admin";
+        public const string DefaultAppConfig = "appsettings.json";
+        public const string DefaultConnectionString = "DefaultConnection";
     }
 }
